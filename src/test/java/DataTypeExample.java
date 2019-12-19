72b9a847621356733d9388dd6f8d3ecbd26bc8a1
Rodrigo Gomez <rodrigo.gomez@overactiveinc.com>
2019-12-19 17:47:00 -0300

    First commit.

---
+++ src/test/java/DataTypeExample.java
@@ -0,0 +1,34 @@
+public class DataTypeExample {
+
+    /**
+    * This is an example class
+     * */
+
+
+
+    public void displayCarInfo(String brand, String chasisNum) {
+
+        System.out.print("");
+    }
+
+
+
+    public static void main(String[] args) {
+
+        /* Car attributes */
+
+    /* Primitives */
+    int qtyWheels = 0;
+    int qtyLights = 0;
+    int qtyDoors = 0;
+    char prefixDomain = "A";
+    boolean turnedOn = false;
+
+    /* Non primitives */
+        String brand = "Ford";
+        String fulldomain = null;
+
+        final String CHASIS_NUM = "ASDFG12345QWERT";
+
+    }
+}
